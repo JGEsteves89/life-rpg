99bb531c036de3dc5f5b6c602b61c145bb374d44
José Esteves <jose.esteves@gmail.com>
2022-09-18 11:21:32 +0100

    C005: Router view page integration

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 
 import './App.css';
 import Api from './Api.js';
 import Player from './Player.js';
 import QuestBoard from './QuestBoard.js';
+import NavBar from './NavBar.js';
 
 const Pages = {
 	Player: 'Player',
 	Quests: 'Quests',
 	Store: 'Store',
 };
-
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			api: null,
-			selectedPage: Pages.Quests,
 		};
+		this.page = Pages.Player;
 	}
-
+	navBar() {}
 	componentDidMount() {
 		Api.get().then((api) => {
 			this.setState({ api: api });
@@ -46,80 +51,73 @@ export default class App extends React.Component {
 		console.log('Data updated');
 	};
 
-	showSelectPage() {
-		if (this.state.selectedPage === Pages.Player) {
-			return (
-				<Player
-					player={this.state.api.getPlayer()}
-					key={this.state.api._player}
-				/>
-			);
-		} else if (this.state.selectedPage === Pages.Quests) {
-			return (
-				<QuestBoard
-					api={this.state.api}
-					appUpdate={this.handleUpdate}
-					quests={this.state.api.getQuests()}
-					key={this.state.api._questBoard.items}
-				/>
-			);
-		} else {
-			return;
-		}
+	storeView() {
+		return <h1>Hello darkness my old friend</h1>;
 	}
+
 	render() {
 		if (!this.state.api) {
 			return;
 		}
 		const player = this.state.api.getPlayer();
 		return (
-			<Box sx={{ flexGrow: 1 }}>
-				<AppBar position="static">
-					<Toolbar
-						className="bg-primary"
-						sx={{ justifyContent: 'space-between' }}>
-						<Avatar
-							alt="Icons"
-							src={process.env.PUBLIC_URL + '/icons8-shield-arcade-96.png'}
-						/>
-						<Box>
-							<Button
-								sx={{ color: this.isSelectedClass(Pages.Player) }}
-								onClick={() => this.setState({ selectedPage: Pages.Player })}>
-								Player
-							</Button>
-							<Button
-								sx={{ color: this.isSelectedClass(Pages.Quests) }}
-								onClick={() => this.setState({ selectedPage: Pages.Quests })}>
-								Quests
-							</Button>
-							<Button
-								sx={{ color: this.isSelectedClass(Pages.Store) }}
-								onClick={() => this.setState({ selectedPage: Pages.Store })}>
-								Store
-							</Button>
-						</Box>
-						<Box
-							sx={{
-								display: 'flex',
-								flexDirection: 'row',
-								alignItems: 'center',
-								gap: '0.5rem',
-								fontSize: '0.7rem',
-							}}>
-							<Box>
-								<p>Level {Math.floor(player.level)}</p>
-								<p>Coins {player.coins.toFixed(0)}</p>
-							</Box>
+			<Router>
+				<Box sx={{ flexGrow: 1 }}>
+					<AppBar position="static">
+						<Toolbar
+							className="bg-primary"
+							sx={{ justifyContent: 'space-between' }}>
 							<Avatar
-								alt="This is me"
-								src="https://avatars.githubusercontent.com/u/15027131?v=4"
+								alt="Icons"
+								src={process.env.PUBLIC_URL + '/icons8-shield-arcade-96.png'}
 							/>
-						</Box>
-					</Toolbar>
-				</AppBar>
-				{this.showSelectPage()}
-			</Box>
+							<NavBar></NavBar>
+							<Box
+								sx={{
+									display: 'flex',
+									flexDirection: 'row',
+									alignItems: 'center',
+									gap: '0.5rem',
+									fontSize: '0.7rem',
+								}}>
+								<Box>
+									<p>Level {Math.floor(player.level)}</p>
+									<p>Coins {player.coins.toFixed(0)}</p>
+								</Box>
+								<Avatar
+									alt="This is me"
+									src="https://avatars.githubusercontent.com/u/15027131?v=4"
+								/>
+							</Box>
+						</Toolbar>
+					</AppBar>
+					<Routes>
+						<Route
+							path="/questBoard"
+							element={
+								<QuestBoard
+									api={this.state.api}
+									appUpdate={this.handleUpdate}
+									quests={this.state.api.getQuests()}
+									key={this.state.api._questBoard.items}>
+									{(this.page = Pages.Player)}
+								</QuestBoard>
+							}
+						/>
+						<Route path="/store" element={<>Hello Darkness my old friend</>} />
+						<Route
+							path="/player"
+							element={
+								<Player
+									player={this.state.api.getPlayer()}
+									key={this.state.api._player}
+								/>
+							}
+						/>
+						<Route path="*" element={<Navigate to="/player" replace />} />
+					</Routes>
+				</Box>
+			</Router>
 		);
 	}
 }
---
+++ src/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import { withRouter } from './withRouter.js';
+import { NavLink } from 'react-router-dom';
+
+class NavBar extends React.Component {
+	render() {
+		return (
+			<Box>
+				<NavLink
+					to="/player"
+					className={(navData) =>
+						'nav-link' + (!navData.isActive ? ' unselected' : '')
+					}>
+					PLAYER
+				</NavLink>
+				<NavLink
+					to="/questboard"
+					className={(navData) =>
+						'nav-link' + (!navData.isActive ? ' unselected' : '')
+					}>
+					QUESTS
+				</NavLink>
+				<NavLink
+					to="/store"
+					className={(navData) =>
+						'nav-link' + (!navData.isActive ? ' unselected' : '')
+					}>
+					STORE
+				</NavLink>
+			</Box>
+		);
+	}
+}
+export default withRouter(NavBar);
